fe0f8d1739f81771daa34ec50751e55f481cce27
Esakkipandi0214 <praveenpandian01@gmail.com>
2024-07-14 15:18:58 +0530

    notificatoinupdateChanges

--- twilio.ts
+++ twilio.ts
@@ -1,36 +1,40 @@
+// utils/twilio.ts
 import twilio from 'twilio';
 require('dotenv').config();
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
-const client = twilio(accountSid, authToken); 
+const defaultPhoneNumber = process.env.TWILIO_PHONE_NUMBER;
 
 const accountSid2 = process.env.TWILIO_ACCOUNT_SID_MY;
 const authToken2 = process.env.TWILIO_AUTH_TOKEN_MY;
 const twilioPhoneNumber = process.env.TWILIO_PHONE_NUMBER_MY;
+
+const client = twilio(accountSid, authToken);
 const client2 = twilio(accountSid2, authToken2);
 
 const sendMessage = (to: string, body: string) => {
   return client.messages.create({
     body,
-    from: process.env.TWILIO_PHONE_NUMBER!,
+    from: defaultPhoneNumber!,
     to,
   });
 };
 
-// Function to send WhatsApp message using the specified client
-const sendWhatsAppMessage = async (client: twilio.Twilio, to: string, message: string) => {
+const sendWhatsAppMessage = async (to: string, message: string, useClient2: boolean = false) => {
+  const selectedClient = useClient2 ? client2 : client;
+
   try {
-    const response = await client.messages.create({
+    const response = await selectedClient.messages.create({
       body: message,
-      from: `whatsapp:${twilioPhoneNumber}`,
+      from: `whatsapp:${twilioPhoneNumber}`, // Use the specific WhatsApp number
       to: `whatsapp:${to}`,
     });
     return { success: true, response };
-  } catch (error: any) { // Specify 'error' as 'any' type or 'Error' explicitly
+  } catch (error: any) {
     console.error('Error sending WhatsApp message:', error);
     return { success: false, error: error.message };
   }
 };
 
-export { sendMessage, sendWhatsAppMessage,client2 };
+export { sendMessage, sendWhatsAppMessage };
